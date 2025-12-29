842aaaea6e6817a6a48cdb7309d43ad6434733ff
Benediktus Satriya Mukti <benediktus_mukti@circles.asia>
2025-12-29 14:04:47 +0700

    feat: Update site domain from kubeletto.io to kubeletto.com and example app URLs to kubeletto.app.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://kubeletto.io',
+  site: 'https://kubeletto.com',
   integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
